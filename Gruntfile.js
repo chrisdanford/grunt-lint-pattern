c3851a12dec5b192e7512548eee4476f2e9f48f3
Chris Danford <chris@pinterest.com>
2013-09-12 13:22:42 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+/*
+ * grunt-find-pattern
+ * https://github.com/chrisdanford/grunt-find-pattern
+ *
+ * Copyright (c) 2013 Chris Danford
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Configuration to be run (and then tested).
+    find_pattern: {
+      matches: {
+        options: {
+          pattern: /Testing/,
+        },
+        files: {
+          src: [
+            'test/fixtures/testing'
+          ],
+        },
+      },
+      does_not_match: {
+        options: {
+          pattern: /Turnip/,
+        },
+        files: {
+          src: [
+            'test/fixtures/testing'
+          ],
+        },
+      },
+      no_pattern_option: {
+        options: {},
+        files: {
+          src: [
+            'test/fixtures/testing'
+          ],
+        },
+      },
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['find_pattern']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/find_pattern.js
@@ -0,0 +1,43 @@
+/*
+ * grunt-find-pattern
+ * https://github.com/chrisdanford/grunt-find-pattern
+ *
+ * Copyright (c) 2013 Chris Danford
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('find_pattern', 'Find a patter in files.  Error on a match.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({});
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        var src = grunt.file.read(filepath);
+        if (!options.pattern) {
+          grunt.warn('The "pattern" option must be specified.');
+        }
+        if (src.match(options.pattern)) {
+          grunt.warn('File "' + filepath + '" matches the pattern.');
+        }
+      });
+    });
+  });
+};
